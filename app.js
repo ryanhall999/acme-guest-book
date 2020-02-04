e03f3a0b2801e4da3f7bda716a55fa53b0a87726
Ryan Hall <ryanhall999@gmail.com>
2020-02-03 20:44:38 -0500

    2nd

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hi ryan');
+const { useEffect, useState } = React;
+
+export default function App() {
+	const [guests, setGuests] = useState([]);
+	useEffect(() => {
+		fetch("./api/guests").then(response => {
+			setGuests(response);
+		});
+	});
+	return (
+		<div>
+			<ul>
+				{guests.map(guest => (
+					<li>guest.name</li>
+				))}
+			</ul>
+		</div>
+	);
+}
