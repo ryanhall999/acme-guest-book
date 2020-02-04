6e3f4a050db5f28f63997af8fe58054a1456e9b9
Kelli Warner <kbwarner@gmail.com>
2020-02-03 21:04:21 -0500

    kelli again

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const { useEffect, useState } = React;
 
-export default function App() {
-	const [guests, setGuests] = useState([]);
-	useEffect(() => {
-		fetch("./api/guests").then(response => {
-			setGuests(response);
-		});
-	});
-	return (
-		<div>
-			<ul>
-				{guests.map(guest => (
-					<li>guest.name</li>
-				))}
-			</ul>
-		</div>
-	);
+function App() {
+  const [guests, setGuests] = useState([]);
+  useEffect(() => {
+    fetch('./api/guests').then(response => {
+      setGuests(response);
+    });
+  });
+  return (
+    <div>
+      <ul>
+        {guests.map(guest => (
+          <li>guest.name</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default App;
